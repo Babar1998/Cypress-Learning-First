20ae02a824c9b290634862e28846567624348a75
Babar Arif <babararifmasood98@gmail.com>
2026-02-03 15:37:15 +0400

    Forms & Aliases

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -1,12 +1,28 @@
 describe('form tests', () => {
     beforeEach(() => {
         cy.visit('/forms')
+        cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
     })
     it('Test subscribe form', () => {
         cy.contains(/testing forms/i)
-        cy.getDataTest('subscribe-form').find('input').type('babar@gmail.com')
+        cy.get('@subscribe-input').type('babar@gmail.com')
         cy.contains(/Successfully subbed: babar@gmail.com!/i).should('not.exist')
         cy.getDataTest('subscribe-button').click()
         cy.contains(/Successfully subbed: babar@gmail.com!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/Successfully subbed: babar@gmail.com!/i).should('not.exist')
+
+        cy.get('@subscribe-input').type('babar@gmail.io')
+        cy.contains(/Invalid email: babar@gmail.io!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/Invalid email: babar@gmail.io!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/Invalid email: babar@gmail.io!/i).should('not.exist')
+
+        cy.contains(/fail!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/fail!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/fail!/i).should('not.exist')
     })
 })
